5a3c1c7cfb612f9c804859eec9da62f348e4bf3f
georgy <georgy.golubev@gmail.com>
2014-06-20 11:35:15 -0700

    Initial Commit

---
+++ UIElements/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIElements
+//
+//  Created by Parallelogramist on 6/20/14.
+//  Copyright (c) 2014 Oscicoil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
